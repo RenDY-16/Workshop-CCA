5cdcc8385933b917eab2b667023bf3d2b351ce04
RenDY-16 <refazym@gmail.com>
2026-08-09 09:19:49 +0800

    DAY 11

--- DAY 11 Struktur Data/test.cpp
+++ DAY 11 Struktur Data/test.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
+struct Produk {
+    string Kode;
+    string nama;
+    float harga;
+    int stok;
+};
 
 int main() {
     
-    const int size = 10;
-    int a[size];
+    int size = 0;
+
+    cout << "Masukkan Jumlah Data: ";
+    cin >> size;
+    Produk a[size];
     
-    for(int i = 0; i < 11; i++) {
-        cout << "Masukkan Nilai Ke-[" << i + 1<< "]: ";
-        cin >> a[i];
+    for(int i = 0; i < size; i++) {
+        cout << "\nMasukkan Data Produk Ke-[" << i + 1 << "]: " << endl;
+        cout << "Kode: ";
+        cin >> a[i].Kode;
+        cin.ignore();
+        cout << "Nama: ";
+        getline(cin, a[i].nama);
+        cin.ignore();
+        cout << "Harga: ";
+        cin >> a[i].harga;
+        cout << "Stok: ";
+        cin >> a[i].stok;
     }
 
 
     cout << "\nHasil Input Nilai Array: \n\n";
-    for(int i = 0; i < 11; i++) {
-        cout << "Nilai Ke-[" << i + 1 << "] = " << a[i] << endl;
+    for(int i = 0; i < size; i++) {
+        cout << "Kode: " << a[i].Kode << endl;
+        cout << "Nama: " << a[i].nama << endl;
+        cout << "Harga: " << a[i].harga << endl;
+        cout << "Stok: " << a[i].stok << endl;
+        cout << "------------------------------" << endl;
     }
-
-
+    return 0;
 }
